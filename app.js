df64ec19177f0d09f9a4d6c61800faa4b793ae2b
Soumya Dey <soumyadey200@gmail.com>
2023-12-07 21:50:22 +0530

    add minor updates

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { spawn } = require('child_process');
+const fs = require('fs');
 const path = require('path');
 const cron = require('node-cron');
 
@@ -9,17 +10,28 @@ Basic mongo dump and restore commands, they contain more options you can have a 
 */
 
 const DB_NAME = 'brancs';
-const ARCHIVE_PATH = path.join(__dirname, 'backup', `${DB_NAME}.gzip`);
 
 // 1. Cron expression for every 5 seconds - */5 * * * * *
 // 2. Cron expression for every night at 00:00 hours (0 0 * * * )
 // Scheduling the backup every day at 00:00
-cron.schedule('0 0 * * *', () => createMongoDump(DB_NAME, ARCHIVE_PATH));
+cron.schedule('0 0 * * *', () => createMongoDump(DB_NAME));
 
-const createMongoDump = (db = 'brancs', archivePath) => {
+const createMongoDump = (db = 'brancs') => {
+  const FOLDER_PATH = path.join(
+    __dirname,
+    'backup',
+    new Date().toLocaleDateString()
+  );
+  const ARCHIVE_PATH = path.join(
+    FOLDER_PATH,
+    `${DB_NAME}_${new Date().toLocaleDateString()}.gzip`
+  );
+  if (!fs.existsSync(FOLDER_PATH))
+    fs.mkdirSync(FOLDER_PATH, { recursive: true });
+
   const child = spawn('mongodump', [
     `--db=${db}`,
-    `--archive=${archivePath}`,
+    `--archive=${ARCHIVE_PATH}`,
     '--gzip',
   ]);
 
